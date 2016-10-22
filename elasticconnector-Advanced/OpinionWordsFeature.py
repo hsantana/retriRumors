98f18b0eb13d3940100df1e07c6be709217ded12
Vishal Chouksey <vkchouksey@gmail.com>
2016-10-23 00:30:04 +0200

    Update OpinionWordsFeature.py

--- elasticconnector-Advanced/OpinionWordsFeature.py
+++ elasticconnector-Advanced/OpinionWordsFeature.py
@@ -29,4 +29,4 @@ def Opinions(para):
                         rating = rating - 1
     return rating
 
-print (Opinions(para="What can I say about this place. The staff of the restaurant is nice and the eggplant is not bad. Apart from that, very uninspired food, lack of atmosphere and too expensive. I am a staunch vegetarian and was sorely dissapointed with the veggie options on the menu. Will be the last time I visit, I recommend others to avoid"))
+print (Opinions(para="I am bad man I am good man"))
