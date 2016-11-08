d9170b332ff12388294520ca3dfd6794c03fe290
Vishal Chouksey <vkchouksey@gmail.com>
2016-11-08 17:47:52 +0100

    changes over directory path and few editing
    Made few changes over directory path and few editing

--- avancedFeatures/NegativePositiveWordsFeature.py
+++ avancedFeatures/NegativePositiveWordsFeature.py
@@ -5,7 +5,7 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 #Making current path in operating system
-os.chdir('/Users/vishalnayanshi/rumorvenv/RumorAnalytics/elasticconnector-Advanced')
+os.chdir('/Users/vishalnayanshi/Sentiment/')
 
 def Opinionschecker(tweets):
     sentense = word_tokenize(tweets)
@@ -25,9 +25,5 @@ def Opinionschecker(tweets):
                     if row[1] == 'pos':
                         Rating = 'positive'
                     elif row[1] == 'neg':
-                        Rating = 'negative'                  
+                        Rating = 'negative'                        
     return Rating
-
-# # For Testing Pusposes
-# train_data = 'What can I say about this place. The staff of the restaurant is nice and the eggplant is not bad. Apart from that, very uninspired food, lack of atmosphere and too expensive. I am a staunch vegetarian and was sorely dissapointed with the veggie options on the menu. Will be the last time I visit, I recommend others to avoid'
-# print Opinionschecker(train_data)
