92088850e3c038756df769f95bb97cdd66412fb0
Vishal Chouksey <vkchouksey@gmail.com>
2016-11-09 15:06:01 +0100

    Deleting non necessary files

--- sentimentAnalysisFeatures/Sentencelevelfeature.py
+++
@@ -1,5 +0,0 @@
-import sys
-from textstat.textstat import textstat
-
-
-# Method for Calulating Sentence Related task like Grammatical Complexity, Average Sentence Complexity and Inferring and Tentative Tweets
